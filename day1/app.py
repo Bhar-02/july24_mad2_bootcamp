3805471a28842169bd8a2d766c50a1306c5387e0
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2024-07-19 19:52:16 +0530

    Changes to be committed: 	modified:   day1/app.py 	new file:   day1/models.py 	new file:   day1/requirements.txt 	modified:   day1/templates/anything.html

--- day1/app.py
+++ day1/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/hello_world')
+@app.route('/hello_world', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'POST':
+        data = request.form
+        name = data['name']
+        desc = data['desc']
+        print(name, desc)
+        return 'You are using POST method'
     var1 = "hello mad2 bootcamp people"
     return render_template('anything.html', var2=var1)
 
