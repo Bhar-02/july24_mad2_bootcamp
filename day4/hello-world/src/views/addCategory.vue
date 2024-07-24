d9b408dbc627af4d3322196bc630d409280f3567
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2024-07-24 21:05:08 +0530

    modified:   day4/hello-world/package-lock.json 	modified:   day4/hello-world/package.json 	modified:   day4/hello-world/src/views/addCategory.vue

--- day4/hello-world/src/views/addCategory.vue
+++ day4/hello-world/src/views/addCategory.vue
@@ -1,12 +1,35 @@
 <template>
     <form>
-        <input type="text" name="name" id="name" placeholder="name field">
-        <input type="text" name="desc" id="desc" placeholder="desc field">
-        <button>add category</button>
+        <input type="text" name="name" id="name" placeholder="name field" v-model="this.name">
+        <input type="text" name="desc" id="desc" placeholder="desc field" v-model="this.desc">
+        <button type="button" @click="printonconsole">add category</button>
     </form>
+    <h1>{{ this.name }}
+    {{ this.desc }}</h1>
+    
+
 </template>
+
 <script>
+// from flask_restful import Resource
+import axios from 'axios';
 export default {
-    name: 'AddCategory'
+    name: 'AddCategory',
+    data() {
+        return {
+            name: null,
+            desc: null
+        }
+    },
+    methods: {
+        printonconsole() {
+            console.log("name: ", this.name);
+            console.log("desc: ", this.desc);
+    }
+}
+
 }
 </script>
+
+<style>
+</style>
